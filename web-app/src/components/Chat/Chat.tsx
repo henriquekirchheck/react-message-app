eddbe2a79107a03c72a56bb52b52902438930051
Henrique Kirch Heck <henriquekirchheck@gmail.com>
2022-02-24 23:35:28 -0300

    removed @types/socket.io

--- web-app/src/components/Chat/Chat.tsx
+++ web-app/src/components/Chat/Chat.tsx
@@ -2,15 +2,11 @@ import { Messages } from './Messages'
 import { SendMessage } from './SendMessage'
 import ChatStyles from './style.module.css'
 
-
-
-
 export function Chat() {
-  
   return (
     <div className={ChatStyles.chat}>
-      <Messages/>
-      <SendMessage/>
+      <Messages />
+      <SendMessage />
     </div>
   )
 }
